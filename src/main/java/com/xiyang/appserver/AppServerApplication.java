318f15d5113eee069e6a681f93641379484cf9a9
qiushui <qiushui@wacai.com>
2018-04-18 15:18:37 +0800

    init

---
+++ src/main/java/com/xiyang/appserver/AppServerApplication.java
@@ -0,0 +1,12 @@
+package com.xiyang.appserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServerApplication.class, args);
+	}
+}
